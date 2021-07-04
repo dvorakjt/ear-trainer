149c8ad118bb30722914d267178e0c951864935f
dvorakjt <dvorakjt@gmail.com>
2021-07-04 01:43:46 -0400

    hook spacer component into native-base theme

--- src/components/spacers/spacer.js
+++ src/components/spacers/spacer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View } from "react-native";
+import { useTheme } from "native-base";
 
 export const Spacer = ({ children }) => {
+  const spacing = useTheme().space;
   return <View style={{ padding: 10 }}>{children}</View>;
 };
--- src/features/training/components/music-view.component.js
+++ src/features/training/components/music-view.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Square, Box } from "native-base";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import {
   ReactNativeSVGContext,
   NotoFontPack,
